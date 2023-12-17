16591885902cba5dae83fc3babc700bfd6796ae4
Hepnerd <20031350+Hepnerd@users.noreply.github.com>
2023-12-16 22:26:06 -0500

    Updated code

--- cardgame/src/PlaySpace/Cards/Cards.jsx
+++ cardgame/src/PlaySpace/Cards/Cards.jsx
@@ -3,7 +3,12 @@ import './Cards.scss'
 export default function Cards({cssClass}) {
     return (
         <>
-            <div className={cssClass}></div>
+            <div className={cssClass+"Card"}>
+                <div className={cssClass+"Content"}>
+                    <div className={cssClass+"Front"}>Front Card</div>
+                    <div className={cssClass+"Back"}>Back Stats</div>
+                </div>
+            </div>
         </>
     )
 }
